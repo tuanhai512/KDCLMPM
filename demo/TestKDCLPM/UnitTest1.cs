9957fae05b00273cc5be240ffee038cc94156520
Hai Tran Tuan <59082486+tuanhai512@users.noreply.github.com>
2021-11-04 23:55:14 +0700

    4/11

---
+++ demo/TestKDCLPM/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestKDCLPM
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int id = 1;
+            string employee_name = "acs";
+            int employee_salary = 12;
+            int employee_age = 30;
+            string profile_image = "ddd";
+
+        }
+    }
+}
---
+++ demo/demo/Program.cs
@@ -0,0 +1,46 @@
+﻿using RestSharp;
+using System.Collections.Generic;
+using Newtonsoft;
+using Newtonsoft.Json;
+using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            getEmployeeData();
+        }
+        public static void getEmployeeData()
+        {
+            var client = new RestClient("http://dummy.restapiexample.com/api/v1/");
+            var request = new RestRequest("employees");
+            var response = client.Execute(request);
+            
+            if(response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                string rawResponse = response.Content;
+                Root result = JsonConvert.DeserializeObject<Root>(rawResponse);
+                Console.WriteLine("{0}", rawResponse);
+            }   
+         
+        }
+        
+    }
+    public class Datum
+    {
+        public int id { get; set; }
+        public string employee_name { get; set; }
+        public int employee_salary { get; set; }
+        public int employee_age { get; set; }
+        public string profile_image { get; set; }
+    }
+
+    public class Root
+    {
+        public string status { get; set; }
+        public List<Datum> data { get; set; }
+    }
+
+}
